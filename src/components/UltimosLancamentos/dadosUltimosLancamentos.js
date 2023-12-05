c20f6bc4fead1ec80a525ab8f9981cd9f8c66757
Noronhajr13 <noronhajf22@gmail.com>
2023-12-05 00:45:20 -0300

    Aula 5 - Props - Curso finalizado

--- src/components/UltimosLancamentos/dadosUltimosLancamentos.js
+++ src/components/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -4,5 +4,6 @@ export const livros = [
   { nome: 'Mongo DB', id: 1, src:livro},
   { nome: 'JavaScript', id: 2, src:livro},
   { nome: 'React', id: 3, src:livro},
-  { nome: 'Node JS', id: 4, src:livro},
+
 ]
+
